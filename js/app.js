74595e252e0e084aee8819805467a17c97282fc7
ivan.curkovic <ivan.curkovic@gmail.com>
2014-08-02 23:56:59 +0200

    fixed problem with timer and no guess

--- js/app.js
+++ js/app.js
@@ -54,7 +54,7 @@ $(document).ready(function () {
         // Reload maps to refresh coords
         svinitialize();
         mminitialize();
-        rminitialize();
+
         // Reset Timer
         resetTimer();
     });
